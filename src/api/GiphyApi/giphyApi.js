ad2d719cc2974d78b94c42580c8d5b2ae774c2e4
Pranay Jeurkar <pranay.j@chegg.com>
2022-02-27 21:23:26 +0530

    Final Commit

--- src/api/GiphyApi/giphyApi.js
+++ src/api/GiphyApi/giphyApi.js
@@ -4,9 +4,8 @@ const LIMIT = 25;
 const HOST_NAME = "giphy";
 const SEARCH_API = "search";
 const TREANDING_API = "trending";
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const API_KEY = "44A7F4nvMFHKr7lYMQIp4KJv0XR5bvm4";
 const URL = getUrl(HOST_NAME);
-
 
 export const getSearchedGifs = (query, offset) => {
     return axios.get(`${URL}/search?api_key=${API_KEY}&q=${query}&limit=${LIMIT}&offset=${offset}&rating=g&lang=en`,
@@ -17,7 +16,6 @@ export const getSearchedGifs = (query, offset) => {
 };
 
 export const getTrendingGifs = (offset) => {
-    console.log(URL, API_KEY, getTimeout(HOST_NAME, TREANDING_API));
     return axios.get(`${URL}/trending?api_key=${API_KEY}&limit=${LIMIT}&rating=g&offset=${offset}`,
         {
             timeout: getTimeout(HOST_NAME, TREANDING_API),
